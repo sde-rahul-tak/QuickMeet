6cb4b9bcbb49d8a784ab90d82d8a1d0473031c11
Rahul Tak <codebase.rahul@gmail.com>
2026-03-29 09:36:07 +0530

    solve error of activity

--- Backend/src/routes/users.rout.js
+++ Backend/src/routes/users.rout.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.route("/login").post(login);
 router.route("/register").post(register);
-router.route("/add_to_activity");
-router.route("/get_all_activity");
+router.route("/add_to_activity").post(addToActivity);
+router.route("/get_all_activity").get(getAllActivity);
 
 export default router;
